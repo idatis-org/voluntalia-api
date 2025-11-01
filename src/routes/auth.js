843c092df0db07f32655017e8113cc2e56958356
Julio Gonzalez <juliojose.gonzalez@gmail.com>
2025-11-01 20:16:38 +0100

    feat: Send email when a new volunteer is created and reset password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const { requireAuth } = require('../middleware/auth');
-const {register, login, refresh, logout, me} = require('../controllers/authController');
+const {
+  register,
+  login,
+  refresh,
+  logout,
+  me,
+  resetPassword,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 // Public routes (no authentication required)
-router.post('/register', register);         // * Registration of new users
-router.post('/login', login);               // * Inicio de sesión con credenciales
-router.post('/refresh', refresh);           // ? Renueva el token de acceso usando
-        
+router.post('/register', register); // * Registration of new users
+router.post('/login', login); // * Inicio de sesión con credenciales
+router.post('/refresh', refresh); // ? Renueva el token de acceso usando
+// Reset password
+router.post('/reset-password', resetPassword);
 // Protected routes (authentication         required)
-router.post('/logout', logout);             // * Log out and invalidate tokens
-router.get('/me', requireAuth, me);         // ? Obtains information about the authenticated user
+router.post('/logout', logout); // * Log out and invalidate tokens
+router.get('/me', requireAuth, me); // ? Obtains information about the authenticated user
 
 module.exports = router;
